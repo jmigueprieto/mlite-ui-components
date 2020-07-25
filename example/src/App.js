cf17520781b9cf2936da16eb48a6e7df3ad1404e
Miguel Prieto <hello@mprieto.me>
2020-07-25 13:16:21 -0400

    Cleanup
    - Fixed a warning
    - Changes in example App

--- example/src/App.js
+++ example/src/App.js
@@ -19,14 +19,18 @@ const App = () => {
         <Instruction>DUP</Instruction>
         <Instruction>INVOKESPECIAL HomePage.&lt;init&gt; ()V</Instruction>
         <Instruction>
-          LDC <Literal>"Hello Worldoss!"</Literal>
+          LDC <Literal>"¡Aló Mundillo!"</Literal>
         </Instruction>
         <Instruction>
           INVOKEVIRTUAL HomePage.publish (Ljava/lang/String;)V
         </Instruction>
       </Bytecode>
-      <Social type='twitter' href='https://twitter.mprieto.me' />
-      <Social type='github' href='https://github.com/jmigueprieto' />
+      <Social type="twitter" href="https://twitter.com/jmigueprieto" />
+      <Social
+        type="github"
+        href="https://github.com/jmigueprieto"
+        target="_blank"
+      />
     </Container>
   )
 }
